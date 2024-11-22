e28a6b2fa2e751334aa1288128d799e2032a432e
aldrichseth <124014743+aldrichseth@users.noreply.github.com>
2024-11-22 03:02:51 -0600

    Update upload.js

--- Project/server/upload.js
+++ Project/server/upload.js
@@ -13,6 +13,9 @@ const config = {
   password: 'b4y3r83}9',
   server: 'bayer-mssql.database.windows.net',
   database: 'mySampleDatabase',
+  authentication: {
+    type: 'default'
+  },
   options: {
     encrypt: true,
     trustServerCertificate: true,
